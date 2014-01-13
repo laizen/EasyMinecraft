eddce6d09693b6b719214f820f91a8556aeca865
Pedro Matias <b13b41@gmail.com>
2014-01-13 12:22:45 -0800

    Update EasyScript.sh

--- EasyScript.sh
+++ EasyScript.sh
@@ -2,4 +2,9 @@
 
 # Author: Pedro Matias                                          #
 # E-mail: b13b41@gmail.com                                      #
-# Versão: 0.1                                                   #             
+# Versão: 0.1                                                   # 
+cd /
+mkdir Minecraft
+cd Minecraft
+wget http://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
+exit
