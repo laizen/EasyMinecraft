10e5362cfd023ec3e682dd26d6dd0af264500e77
Pedro Matias <b13b41@gmail.com>
2014-01-13 13:00:10 -0800

    Update EasyScript.sh

--- EasyScript.sh
+++ EasyScript.sh
@@ -4,12 +4,17 @@
 # E-mail: b13b41@gmail.com                                      #
 # Versão: 0.1                                                   # 
 cd /
+echo "Criando Pastas..."
 mkdir minecraftserver
 cd minecraftserver
 mkdir backup
+echo "Baixando Aquivos do Repositorio..."
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
 wget https://raw.github.com/laizen/EasyMinecraft/master/minecraft
+echo "Registrando Serviço"
 cp minecraft /etc/init.d/
-chmod a+x minecraft
-update.rc minecraft defaults
+chmod a+x /etc/init.d/minecraft
+update-rc.d minecraft defaults
+echo "Fim da Instalação"
+sleep 10
 exit
