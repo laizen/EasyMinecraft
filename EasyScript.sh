41dc981636e2117ecd325f984b7294036e1c54d4
Pedro Matias <b13b41@gmail.com>
2014-01-13 12:57:17 -0800

    Update EasyScript.sh

--- EasyScript.sh
+++ EasyScript.sh
@@ -4,7 +4,12 @@
 # E-mail: b13b41@gmail.com                                      #
 # Versão: 0.1                                                   # 
 cd /
-mkdir Minecraft
-cd Minecraft
-wget http://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
+mkdir minecraftserver
+cd minecraftserver
+mkdir backup
+wget https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar
+wget https://raw.github.com/laizen/EasyMinecraft/master/minecraft
+cp minecraft /etc/init.d/
+chmod a+x minecraft
+update.rc minecraft defaults
 exit
