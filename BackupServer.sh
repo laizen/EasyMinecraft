3634a9bf343a2e9307214e17c352be2c3db4b4d7
Pedro Matias <b13b41@gmail.com>
2014-02-13 06:59:26 -0800

    Update BackupServer.sh

--- BackupServer.sh
+++ BackupServer.sh
@@ -7,10 +7,10 @@ echo " "
 
 dadosfull() {
 
-SRCDIR="/home/jhoni /etc" #diretórios que serão feito backup
-DSTDIR=/mnt/extra #diretório de destino do backup
-DATA=`date +%x-%k%M%S` #pega data atual
-TIME_BKCP=+15 #número de dias em que será deletado o arquivo de backup
+SRCDIR="/home/pedro/newminecraft/wolrd" #diretórios que serão feito backup
+DSTDIR=/var/www/bck #diretório de destino do backup
+DATA=`date +%x-%k.%M.%S` #pega data atual
+TIME_BKCP=+5 #número de dias em que será deletado o arquivo de backup
 
 #criar o arquivo full-data.tar no diretório de destino
 ARQ=$DSTDIR/full-$DATA.tar.gz
@@ -28,16 +28,15 @@ if [ $? -eq 0 ] ; then
         echo "Backup Full concluído com Sucesso"
    DATAFIN=`date +%c`
    echo "Data de termino: $DATAFIN"
-   echo "Backup realizado com sucesso" >> /var/log/backup_full.log
-   echo "Criado pelo usuário: $USER" >> /var/log/backup_full.log
-   echo "INICIO: $DATAIN" >> /var/log/backup_full.log
-   echo "FIM: $DATAFIN" >> /var/log/backup_full.log
-   echo "-----------------------------------------" >> /var/log/backup_full.log
+   echo "Backup realizado com sucesso" >> /var/www/log/backup_full.log
+   echo "Criado pelo usuário: $USER" >> /var/www/log/backup_full.log
+   echo "INICIO: $DATAIN" >> /var/www/log/backup_full.log
+   echo "FIM: $DATAFIN" >> /var/www/log/backup_full.log
+   echo "-----------------------------------------" >> /var/www/log/backup_full.log
    echo " "
-   echo "Log gerado em /var/log/backup_full.log"
-
+   echo "Log gerado em /var/www/log/backup_full.log"
 else
-   echo "ERRO! Backup do dia $DATAIN" >> /var/log/backup_full.log
+   echo "ERRO! Backup do dia $DATAIN" >> /var/www/log/backup_full.log
 fi   
 }
 
@@ -51,6 +50,5 @@ find $DSTDIR -name "f*" -ctime $TIME_BKCP -exec rm -f {} ";"
       echo "Erro durante a busca e destruição do backup antigo!"
    fi
 }
-
 
 exit 0
